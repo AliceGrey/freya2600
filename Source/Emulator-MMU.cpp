310242c7f035d1e342667663623a74c6fd8dee32
Allie <allieroblee@cyberresilience.com>
2023-06-21 19:54:41 -0400

    ADDR_
    Name change

--- Source/Emulator-MMU.cpp
+++ Source/Emulator-MMU.cpp
@@ -13,46 +13,46 @@ uint8_t Emulator::ReadByte(word address)
     // $30-$3D TIA (read)
     if (address >= 0x00 && address <= 0x3D) {
         switch (address & 0x0F) {
-        case CXM0P:  // Read: Collision D7=(M0;P1); D6=(M0,P0)
+        case ADDR_CXM0P:  // Read: Collision D7=(M0;P1); D6=(M0,P0)
             printf("READ CXM0P\n");
             break;
-        case CXM1P:  // Read: Collision D7=(M1;P0); D6=(M1,P1)
+        case ADDR_CXM1P:  // Read: Collision D7=(M1;P0); D6=(M1,P1)
             printf("READ CXM1P\n");
             break;
-        case CXP0FB:  // Read: Collision D7=(P0;PF); D6=(P0,BL)
+        case ADDR_CXP0FB:  // Read: Collision D7=(P0;PF); D6=(P0,BL)
             printf("READ CXP0FB\n");
             break;
-        case CXP1FB:  // Read: Collision D7=(P1;PF); D6=(P1;BL)
+        case ADDR_CXP1FB:  // Read: Collision D7=(P1;PF); D6=(P1;BL)
             printf("READ CXP1FB\n");
             break;
-        case CXM0FB:  // Read: Collision D7=(M0;PF); D6=(M0;BL)
+        case ADDR_CXM0FB:  // Read: Collision D7=(M0;PF); D6=(M0;BL)
             printf("READ CXM0FB\n");
             break;
-        case CXM1FB:  // Read: Collision D7=(M1;PF); D6=(M1;BL)
+        case ADDR_CXM1FB:  // Read: Collision D7=(M1;PF); D6=(M1;BL)
             printf("READ CXM1FB\n");
             break;
-        case CXBLPF:  // Read: Collision D7=(BL;PF); D6=(unused)
+        case ADDR_CXBLPF:  // Read: Collision D7=(BL;PF); D6=(unused)
             printf("READ CXBLPF\n");
             break;
-        case CXPPMM:  // Read: Collision D7=(P0;P1); D6=(M0;M1)
+        case ADDR_CXPPMM:  // Read: Collision D7=(P0;P1); D6=(M0;M1)
             printf("READ CXPPMM\n");
             break;
-        case INPT0:  // Read: Pot port D7
+        case ADDR_INPT0:  // Read: Pot port D7
             printf("READ INPT0\n");
             break;
-        case INPT1:  // Read: Pot port D7
+        case ADDR_INPT1:  // Read: Pot port D7
             printf("READ INPT1\n");
             break;
-        case INPT2:  // Read: Pot port D7
+        case ADDR_INPT2:  // Read: Pot port D7
             printf("READ INPT2\n");
             break;
-        case INPT3:  // Read: Pot port D7
+        case ADDR_INPT3:  // Read: Pot port D7
             printf("READ INPT3\n");
             break;
-        case INPT4:  // Read: P1 joystick trigger: D7
+        case ADDR_INPT4:  // Read: P1 joystick trigger: D7
             printf("READ INPT4\n");
             break;
-        case INPT5:  // Read: P2 joystick trigger: D7
+        case ADDR_INPT5:  // Read: P2 joystick trigger: D7
             printf("READ INPT5\n");
             break;
         default:
@@ -145,139 +145,139 @@ void Emulator::WriteByte(word address, byte data)
     // $30-$3D TIA (read)
     if (address >= 0x00 && address <= 0x2C) {
         switch (address) {
-            case VSYNC:
+            case ADDR_VSYNC:
                 printf("WRITE VSYNC\n");  // Write: VSYNC set-clear (D1)
                 break;
-            case VBLANK:
+            case ADDR_VBLANK:
                 printf("WRITE VBLANK\n");  // Write: VBLANK set-clear (D7-6,D1)
                 break;
-            case WSYNC :
+            case ADDR_WSYNC :
                 printf("WRITE WSYNC\n");  // Write: Wait for leading edge of hrz. blank (strobe)
                 break;
-            case RSYNC:
+            case ADDR_RSYNC:
                 printf("WRITE RSYNC\n");  // Write: Reset hrz. sync counter (strobe)
                 break;
-            case NUSIZ0:
+            case ADDR_NUSIZ0:
                 printf("WRITE NUSIZ0\n");  // Write: Number-size player-missle 0 (D5-0)
                 break;
-            case NUSIZ1:
+            case ADDR_NUSIZ1:
                 printf("WRITE NUSIZ1\n");  // Write: Number-size player-missle 1 (D5-0)
                 break;
-            case COLUP0:
+            case ADDR_COLUP0:
                 printf("WRITE COLUP0\n");  // Write: Color-lum player 0 (D7-1)
                 break;
-            case COLUP1:
+            case ADDR_COLUP1:
                 printf("WRITE COLUP1\n");  // Write: Color-lum player 1 (D7-1)
                 break;
-            case COLUPF:
+            case ADDR_COLUPF:
                 printf("WRITE COLUPF\n");  // Write: Color-lum playfield (D7-1)
                 break;
-            case COLUBK:
+            case ADDR_COLUBK:
                 printf("WRITE COLUBK\n");  // Write: Color-lum background (D7-1)
                 break;
-            case CTRLPF:
+            case ADDR_CTRLPF:
                 printf("WRITE CTRLPF\n");  // Write: Contrl playfield ballsize & coll. (D5-4,D2-0)
                 break;
-            case REFP0:
+            case ADDR_REFP0:
                 printf("WRITE REFP0\n");  // Write: Reflect player 0 (D3)
                 break;
-            case REFP1:
+            case ADDR_REFP1:
                 printf("WRITE REFP1\n");  // Write: Reflect player 1 (D3)
                 break;
-            case PF0:
+            case ADDR_PF0:
                 printf("WRITE PF0\n");  // Write: Playfield register byte 0 (D7-4)
                 break;
-            case PF1:
+            case ADDR_PF1:
                 printf("WRITE PF1\n");  // Write: Playfield register byte 1 (D7-0)
                 break;
-            case PF2:
+            case ADDR_PF2:
                 printf("WRITE PF2\n");  // Write: Playfield register byte 2 (D7-0)
                 break;
-            case RESP0:
+            case ADDR_RESP0:
                 printf("WRITE RESP0\n");  // Write: Reset player 0 (strobe)
                 break;
-            case RESP1:
+            case ADDR_RESP1:
                 printf("WRITE RESP1\n");  // Write: Reset player 1 (strobe)
                 break;
-            case RESM0:
+            case ADDR_RESM0:
                 printf("WRITE RESM0\n");  // Write: Reset missle 0 (strobe)
                 break;
-            case RESM1:
+            case ADDR_RESM1:
                 printf("WRITE RESM1\n");  // Write: Reset missle 1 (strobe)
                 break;
-            case RESBL:
+            case ADDR_RESBL:
                 printf("WRITE RESBL\n");  // Write: Reset ball (strobe)
                 break;
-            case AUDC0:
+            case ADDR_AUDC0:
                 printf("WRITE AUDC0\n");  // Write: Audio control 0 (D3-0)
                 break;
-            case AUDC1:
+            case ADDR_AUDC1:
                 printf("WRITE AUDC1\n");  // Write: Audio control 1 (D4-0)
                 break;
-            case AUDF0:
+            case ADDR_AUDF0:
                 printf("WRITE AUDF0\n");  // Write: Audio frequency 0 (D4-0)
                 break;
-            case AUDF1:
+            case ADDR_AUDF1:
                 printf("WRITE AUDF1\n");  // Write: Audio frequency 1 (D3-0)
                 break;
-            case AUDV0:
+            case ADDR_AUDV0:
                 printf("WRITE AUDV0\n");  // Write: Audio volume 0 (D3-0)
                 break;
-            case AUDV1:
+            case ADDR_AUDV1:
                 printf("WRITE AUDV1\n");  // Write: Audio volume 1 (D3-0)
                 break;
-            case GRP0:
+            case ADDR_GRP0:
                 printf("WRITE GRP0\n");  // Write: Graphics player 0 (D7-0)
                 break;
-            case GRP1:
+            case ADDR_GRP1:
                 printf("WRITE GRP1\n");  // Write: Graphics player 1 (D7-0)
                 break;
-            case ENAM0:
+            case ADDR_ENAM0:
                 printf("WRITE ENAM0\n");  // Write: Graphics (enable) missle 0 (D1)
                 break;
-            case ENAM1:
+            case ADDR_ENAM1:
                 printf("WRITE ENAM1\n");  // Write: Graphics (enable) missle 1 (D1)
                 break;
-            case ENABL:
+            case ADDR_ENABL:
                 printf("WRITE ENABL\n");  // Write: Graphics (enable) ball (D1)
                 break;
-            case HMP0:
+            case ADDR_HMP0:
                 printf("WRITE HMP0\n");  // Write: Horizontal motion player 0 (D7-4)
                 break;
-            case HMP1:
+            case ADDR_HMP1:
                 printf("WRITE HMP1\n");  // Write: Horizontal motion player 1 (D7-4)
                 break;
-            case HMM0:
+            case ADDR_HMM0:
                 printf("WRITE HMM0\n");  // Write: Horizontal motion missle 0 (D7-4)
                 break;
-            case HMM1:
+            case ADDR_HMM1:
                 printf("WRITE HMM1\n");  // Write: Horizontal motion missle 1 (D7-4)
                 break;
-            case HMBL:
+            case ADDR_HMBL:
                 printf("WRITE HMBL\n");  // Write: Horizontal motion ball (D7-4)
                 break;
-            case VDELP0:
+            case ADDR_VDELP0:
                 printf("WRITE VDELP0\n");  // Write: Vertical delay player 0 (D0)
                 break;
-            case VDELP1:
+            case ADDR_VDELP1:
                 printf("WRITE VDELP1\n");  // Write: Vertical delay player 1 (D0)
                 break;
-            case VDELBL:
+            case ADDR_VDELBL:
                 printf("WRITE VDELBL\n");  // Write: Vertical delay ball (D0)
                 break;
-            case RESMP0:
+            case ADDR_RESMP0:
                 printf("WRITE RESMP0\n");  // Write: Reset missle 0 to player 0 (D1)
                 break;
-            case RESMP1:
+            case ADDR_RESMP1:
                 printf("WRITE RESMP1\n");  // Write: Reset missle 1 to player 1 (D1)
                 break;
-            case HMOVE:
+            case ADDR_HMOVE:
                 printf("WRITE HMOVE\n");  // Write: Apply horizontal motion (strobe)
                 break;
-            case HMCLR:
+            case ADDR_HMCLR:
                 printf("WRITE HMCLR\n");  // Write: Clear horizontal motion registers (strobe)
                 break;
-            case CXCLR:
+            case ADDR_CXCLR:
                 printf("WRITE CXCLR\n");  // Write: Clear collision latches (strobe)
                 break;
             default:
